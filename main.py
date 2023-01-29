4ae55a2c1d483d0b82d53d60f799511e89f966f0
Ravshanjon Raxmatullayev <36916470+ravshanjonuz@users.noreply.github.com>
2023-01-29 15:35:21 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import asyncio
 from aiogram import Bot, Dispatcher, executor
 
 loop = asyncio.new_event_loop()
-bot = Bot("", parse_mode='HTML')
+bot = Bot("6027625595:AAHd9zZZ0NdHJe_ztQn7-lsTJGR4hoYTnpM", parse_mode='HTML')
 dp = Dispatcher(bot, loop)
 
 if __name__ == '__main__':
