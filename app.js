df4781c548dddba5d2ee5632f8f5ff89a70318f8
Ravshanjon Raxmatullayev <36916470+ravshanjonuz@users.noreply.github.com>
2023-01-30 15:02:51 +0500

    a

--- app.js
+++ app.js
@@ -12,8 +12,7 @@ let bg = ["a", "b", "c", "d", "e"]
 let arrs = ['1a', '1b', '1c', '1d', '1e', '2a', '2b', '2c', '2d', '2e', '3a', '3b', '3c', '3d', '3e']
 
 for (var i = 0; i < 15; i++) {
-	btn = document.getElementById("btn" + String(i));
-	btn.addEventListener("click", function(){
+	document.getElementById("btn" + String(i)).addEventListener("click", function(){
 		if (tg.MainButton.isVisible) {
 			tg.MainButton.hide();
 		}
