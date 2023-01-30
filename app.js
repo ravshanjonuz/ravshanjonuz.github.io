382a706618e17b91c38aa50858cee13555e516e4
Ravshanjon Raxmatullayev <36916470+ravshanjonuz@users.noreply.github.com>
2023-01-30 14:47:21 +0500

    bb

--- app.js
+++ app.js
@@ -9,18 +9,18 @@ let item = "";
 
 let bg = ["a", "b", "c", "d", "e"]
 
-for (var i = 1; i <= 15; i++) {
-	document.getElementById("btn" + String(i)).addEventListener("click", function(){
+let arrs = ['1a', '1b', '1c', '1d', '1e', '2a', '2b', '2c', '2d', '2e', '3a', '3b', '3c', '3d', '3e']
+
+for (var i = 0; i < 15; i++) {
+	btn = document.getElementById("btn" + String(i));
+	btn.addEventListener("click", function(){
 		if (tg.MainButton.isVisible) {
 			tg.MainButton.hide();
 		}
-		else {
+		else {						
+			tg.MainButton.setText(arrs[i] + "-stul tanlandi");
+			item = String(i+1);
 			tg.MainButton.show();
-			qator = Math.floor((i - 1) / 5) + 1
-			ustun = bg[(i - 1) % 5]
-			
-			tg.MainButton.setText(String(qator) + ustun + "-stul tanlandi");
-			item = String(i);
 		}
 	});
 }
