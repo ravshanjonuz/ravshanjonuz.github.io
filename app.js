564c32b49afa958c07959a67e590d2f122b14dbb
Ravshanjon Raxmatullayev <36916470+ravshanjonuz@users.noreply.github.com>
2023-01-30 13:58:08 +0500

    v

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ array.forEach(element => {
 			qator = (element - 1) / 5 + 1
 			ustun = bg[(element - 1) % 5]
 
-			tg.MainButton.setText(strin(qator) + ustun + "-stul tanlandi");
+			tg.MainButton.setText(string(qator) + ustun + "-stul tanlandi");
 			item = string(element);
 			tg.MainButton.show();
 		}
